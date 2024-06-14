f3915521f127f332d8276452138a8f75c3e8aea5
Sparky_Developer <164982328+vigneshsparky@users.noreply.github.com>
2024-06-14 13:01:17 +0530

    Delete function.js

--- function.js
+++
@@ -1,5 +0,0 @@
-const menubt = document.getElementById("menu_bt");
-const addclass = document.querySelector("header section");
-menubt.addEventListener("click", () => {
-  addclass.ClassList.toggle("show_menu");
-});
